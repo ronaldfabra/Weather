85d35fa5c65ffd54d96ed393598f4b9524677439
Ronal Fabra <ronal.fabra7@gmail.com>
2025-01-30 17:22:14 -0500

    updated workflow

--- run_tests.sh
+++ run_tests.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 SCHEME='WeatherTests'
-
 DESTINATION="platform=iOS Simulator,OS=latest,name=iPhone 16"
 
-xcodebuild test -scheme $SCHEME -destination "$DESTINATION" CODE_SIGNING_ALLOWED='NO' -enableCodeCoverage YES
+RESULT_DIR="./build/test-logs"
+RESULT_BUNDLE="$RESULT_DIR/TestResults.xcresult"
+
+mkdir -p $RESULT_DIR
+
+xcodebuild test -scheme $SCHEME -destination "$DESTINATION" CODE_SIGNING_ALLOWED='NO' -enableCodeCoverage YES -resultBundlePath $RESULT_BUNDLE
