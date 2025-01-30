9929091f6429d8d36dfa43a2e0f319f1cf237d67
Ronal Fabra <ronal.fabra7@gmail.com>
2025-01-30 18:49:21 -0500

    delete unnnecesary codecov code

--- run_tests.sh
+++ run_tests.sh
@@ -3,9 +3,5 @@
 SCHEME='WeatherTests'
 DESTINATION="platform=iOS Simulator,OS=latest,name=iPhone 16"
 
-RESULT_DIR="./build/test-logs"
-RESULT_BUNDLE="$RESULT_DIR/TestResults.xcresult"
-mkdir -p $RESULT_DIR
-
-xcodebuild test -scheme $SCHEME -destination "$DESTINATION" CODE_SIGNING_ALLOWED='NO' -enableCodeCoverage YES -resultBundlePath $RESULT_BUNDLE
+xcodebuild test -scheme $SCHEME -destination "$DESTINATION" CODE_SIGNING_ALLOWED='NO' -enableCodeCoverage YES
 
