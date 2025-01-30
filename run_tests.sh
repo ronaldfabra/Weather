8e138b92f08eb5a3bbf66bfa93e906ad0c1504bb
Ronal Fabra <ronal.fabra7@gmail.com>
2025-01-30 16:44:42 -0500

    updated script to load the coverage

--- run_tests.sh
+++ run_tests.sh
@@ -4,4 +4,4 @@ SCHEME='WeatherTests'
 
 DESTINATION="platform=iOS Simulator,OS=latest,name=iPhone 16"
 
-xcodebuild test -scheme $SCHEME -sdk iphonesimulator -destination "$DESTINATION" CODE_SIGNING_ALLOWED='NO' -enableCodeCoverage YES
+xcodebuild test -scheme $SCHEME -destination "$DESTINATION" CODE_SIGNING_ALLOWED='NO' -enableCodeCoverage YES
