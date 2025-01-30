68d36e208828363989fc471e662876d0807ebb46
Ronal Fabra <ronal.fabra7@gmail.com>
2025-01-30 14:51:28 -0500

    added support to show the coverage in codecov tool

--- run_tests.sh
+++ run_tests.sh
@@ -2,6 +2,6 @@
 
 SCHEME='WeatherTests'
 
-DESTINATION='platform=iOS Simulator,OS=latest,name=iPhone 16'
+DESTINATION='platform=iOS Simulator,OS=18.2,name=iPhone 16'
 
 xcodebuild test -scheme $SCHEME -destination "$DESTINATION" CODE_SIGNING_ALLOWED='NO' -enableCodeCoverage YES
