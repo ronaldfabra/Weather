c7953f6d5b7ba95f9e133809aed47aecbfdbd4c0
Ronal Fabra <ronal.fabra7@gmail.com>
2025-01-30 12:33:56 -0500

    added new script to run the unit tests with github actions
    update the yml file
    
    update the yml file to select the correct xcode
    
    update the yml file to select the correct xcode
    
    update the yml file to select the correct xcode
    
    update the yml file to select the correct xcode
    
    update the yml file to select the correct xcode
    
    updated the script to run the unit tests
    
    update the yml file to select the correct xcode
    
    update the yml file to select the correct xcode

---
+++ run_tests.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+SCHEME='WeatherTests'
+
+DESTINATION='platform=iOS Simulator,OS=latest,name=iPhone 16'
+
+xcodebuild test -scheme $SCHEME -destination "$DESTINATION" CODE_SIGNING_ALLOWED='NO' -enableCodeCoverage YES
