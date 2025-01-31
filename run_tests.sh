d46703336e72e3b674a5d4d4ccf8f68383ceedeb
Ronal Fabra <ronal.fabra7@gmail.com>
2025-01-30 20:40:32 -0500

    updated the script

--- run_tests.sh
+++ run_tests.sh
@@ -8,20 +8,18 @@ IOS_VERSIONS=$(xcrun simctl list runtimes | grep -o 'iOS [0-9]\{1,\}\.[0-9]\{1,\
 # Get the latest version (newest)
 LATEST_VERSION=$(echo "$IOS_VERSIONS" | head -n 1)
 
-# Get the major version (the first number, e.g., 18 from 18.2)
+# Get the major version of the latest version (e.g., 18 from 18.2)
 LATEST_MAJOR_VERSION=$(echo "$LATEST_VERSION" | cut -d '.' -f 1)
 
-# Find the version just before the latest major version (not the same major)
-PREVIOUS_MAJOR_VERSION=$(echo "$IOS_VERSIONS" | grep -o "^[0-9]\{1,\}" | grep -v "^$LATEST_MAJOR_VERSION$" | sort -V | tail -n 1)
+# Get the previous version (second newest)
+PREVIOUS_VERSION=$(echo "$IOS_VERSIONS" | sed -n '2p')
 
-# Construct the version string to use: If last version is 18.x, pick 17.x
-if [[ $LATEST_MAJOR_VERSION == $PREVIOUS_MAJOR_VERSION ]]; then
-    # If the latest version is 18.x, we take the previous major version (17.x)
-    # This ensures you pick a version like 17.x if the last version is 18.x
-    DESTINATION_VERSION="$PREVIOUS_MAJOR_VERSION"
-else
-    # If the latest version is not the same as the major version of the previous release
-    DESTINATION_VERSION="$LATEST_MAJOR_VERSION"
+# If the latest version is 18.x, choose the version before 18 (e.g., 17.x)
+if [[ $LATEST_MAJOR_VERSION == "18" ]]; then
+    # Find the version before 18.x
+    PREVIOUS_MAJOR_VERSION=$(echo "$IOS_VERSIONS" | grep -o "^[0-9]\{1,\}" | grep -v "^18$" | sort -V | tail -n 1)
+    # Construct the version string
+    PREVIOUS_VERSION=$(echo "$IOS_VERSIONS" | grep "^$PREVIOUS_MAJOR_VERSION" | head -n 1)
 fi
 
 # Function to check if there are available simulators for a given iOS version
@@ -49,16 +47,16 @@ function get_first_device_name {
 }
 
 # First, try with the latest iOS version
-if check_simulators_for_version "$LATEST_VERSION"; then
-    echo "Using the latest iOS version: $LATEST_VERSION"
-    DEVICE_NAME=$(get_first_device_name "$LATEST_VERSION")
-    DESTINATION="platform=iOS Simulator,OS=$LATEST_VERSION,name=$DEVICE_NAME"
-else
-    # If there are no devices for the latest version, use the previous major version
-    echo "No devices available for the latest version. Using the previous major version: $DESTINATION_VERSION"
-    DEVICE_NAME=$(get_first_device_name "$DESTINATION_VERSION")
-    DESTINATION="platform=iOS Simulator,OS=$DESTINATION_VERSION,name=$DEVICE_NAME"
-fi
+#if check_simulators_for_version "$LATEST_VERSION"; then
+#    echo "Using the latest iOS version: $LATEST_VERSION"
+#    DEVICE_NAME=$(get_first_device_name "$LATEST_VERSION")
+#    DESTINATION="platform=iOS Simulator,OS=$LATEST_VERSION,name=$DEVICE_NAME"
+#else
+    # If there are no devices for the latest version, use the previous version
+    echo "No devices available for the latest version. Using the previous iOS version: $PREVIOUS_VERSION"
+    DEVICE_NAME=$(get_first_device_name "$PREVIOUS_VERSION")
+    DESTINATION="platform=iOS Simulator,OS=$PREVIOUS_VERSION,name=$DEVICE_NAME"
+#fi
 
 # Run the test command
 xcodebuild test -scheme $SCHEME -destination "$DESTINATION" CODE_SIGNING_ALLOWED='NO' -enableCodeCoverage YES
