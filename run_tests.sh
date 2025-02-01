cf83987f4e348313dca709ffd4bcbc1f9a253ff3
Ronal Fabra <ronal.fabra7@gmail.com>
2025-02-01 09:45:18 -0500

    update workflow to run the tests and push the coverage to codecov
    update workflow to run the tests and push the coverage to codecov
    
    update workflow to run the tests and push the coverage to codecov
    
    update workflow to run the tests and push the coverage to codecov
    
    update workflow to run the tests and push the coverage to codecov
    
    update workflow to run the tests and push the coverage to codecov
    
    update workflow to run the tests and push the coverage to codecov
    
    update workflow to run the tests and push the coverage to codecov
    
    update workflow to run the tests and push the coverage to codecov
    
    update workflow to run the tests and push the coverage to codecov
    
    update workflow to run the tests and push the coverage to codecov
    
    update workflow to run the tests and push the coverage to codecov
    
    update workflow to run the tests and push the coverage to codecov

--- run_tests.sh
+++ run_tests.sh
@@ -4,22 +4,7 @@ UNIT_TEST_SCHEME='WeatherTests'
 UI_TEST_SCHEME='WeatherUITests'
 
 DESTINATION='platform=iOS Simulator,OS=latest,name=iPhone 16'
-OUTPUT_DIR='./test-reports'
 
-# Crear directorio para los reportes de cobertura si no existe
-mkdir -p $OUTPUT_DIR
+xcodebuild test -scheme $UNIT_TEST_SCHEME -destination "$DESTINATION" CODE_SIGNING_ALLOWED='NO' -enableCodeCoverage YES | xcpretty
 
-echo "Running unit tests..."
-xcodebuild test -scheme $UNIT_TEST_SCHEME -destination "$DESTINATION" CODE_SIGNING_ALLOWED='NO' -enableCodeCoverage YES -resultBundlePath  $OUTPUT_DIR/unit_tests.log
-
-echo "Running UI tests..."
-xcodebuild test -scheme $UI_TEST_SCHEME -destination "$DESTINATION" CODE_SIGNING_ALLOWED='NO' -enableCodeCoverage YES -resultBundlePath $OUTPUT_DIR/ui_tests.log
-
-# Mover los reportes de cobertura a un lugar accesible
-cp -r ~/Library/Developer/Xcode/DerivedData/*/Coverage/*.xccovreport $OUTPUT_DIR/
-
-# Opcional: Listar archivos de cobertura para asegurarte de que todo esté bien
-echo "Listing coverage files:"
-ls -l $OUTPUT_DIR
-
-echo "Tests and coverage reports are ready."
+xcodebuild test -scheme $UI_TEST_SCHEME -destination "$DESTINATION" CODE_SIGNING_ALLOWED='NO' -enableCodeCoverage YES | xcpretty
